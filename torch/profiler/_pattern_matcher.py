2d5b83209df7d78c367294ef952c7e39f7178d6f
Taylor Robie <taylorrobie@fb.com>
2022-07-22 13:55:26 -0700

    [Profiler] Add `tag` property to `Result`
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/81965
    
    With a bit of template deduction we can make the variant tell us what type it is, and then we don't have to rely on (and maintain) a bunch of `isinstance` checks in Python.
    ghstack-source-id: 162345291
    
    Differential Revision: [D38066829](https://our.internmc.facebook.com/intern/diff/D38066829/)

--- torch/profiler/_pattern_matcher.py
+++ torch/profiler/_pattern_matcher.py
@@ -6,7 +6,6 @@ import torch
 from torch.profiler import profile
 from torch.profiler._utils import index_of_first_match
 from torch._C._autograd import (_ProfilerEvent, _ExtraFields_TorchOp,
-                                _ExtraFields_Backend, _ExtraFields_Allocation,
                                 _ExtraFields_PyCCall, _ExtraFields_PyCall,
                                 _EventType)
 
@@ -235,7 +234,7 @@ class FP32MatMulPattern(Pattern):
 
     def match(self, event: _ProfilerEvent):
         # If we saw this pattern once, we don't need to match it again
-        if event_type(event) != _EventType.TorchOp:
+        if event.tag != _EventType.TorchOp:
             return False
         assert isinstance(event.extra_fields, _ExtraFields_TorchOp)
         if event.name() == "aten::mm":
@@ -249,8 +248,7 @@ class FP32MatMulPattern(Pattern):
 
 def source_code_location(event: _ProfilerEvent):
     while event:
-        if event_type(event) == _EventType.PyCall or event_type(
-                event) == _EventType.PyCCall:
+        if event.tag == _EventType.PyCall or event.tag == _EventType.PyCCall:
             assert isinstance(event.extra_fields,
                               _ExtraFields_PyCall) or isinstance(
                                   event.extra_fields, _ExtraFields_PyCCall)
@@ -273,18 +271,3 @@ def report_all_anti_patterns(prof):
             if report_msg not in reported:
                 print(report_msg)
                 reported.add(report_msg)
-
-
-def event_type(event: _ProfilerEvent):
-    if isinstance(event.extra_fields, _ExtraFields_TorchOp):
-        return _EventType.TorchOp
-    elif isinstance(event.extra_fields, _ExtraFields_Backend):
-        return _EventType.Backend
-    elif isinstance(event.extra_fields, _ExtraFields_Allocation):
-        return _EventType.Allocation
-    elif isinstance(event.extra_fields, _ExtraFields_PyCall):
-        return _EventType.PyCall
-    elif isinstance(event.extra_fields, _ExtraFields_PyCCall):
-        return _EventType.PyCCall
-    else:
-        raise Exception("Unknown event type")
